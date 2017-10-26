f2f16faa85a9fce8227fe8029ef248ced219feb0
mguillaumot <mathilde.guillaumot@club.fr>
2017-10-26 10:00:27 +0200

    Update on logistic regression methods

--- scripts/implementations.py
+++ scripts/implementations.py
@@ -115,104 +115,97 @@ def ridge_regression(y, tx, lambda_):
 
 
 
-"""
-Logistic regression
-"""
+""" Logistic regression using gradient descent """
+
 def sigmoid(t):
     """apply sigmoid function on t."""
-    #return np.exp(t)/(1+np.exp(t))
-    #return 1/(1+np.exp(-t))
     return np.exp(-np.logaddexp(0, -t))
 
 def calculate_loss(y, tx, w):
     """compute the cost by negative log likelihood."""
-    """pred = sigmoid(tx.dot(w))
-    loss = y.T.dot(np.log(pred))+(1-y).T.dot(np.log(1-pred))
-    return np.squeeze(-loss)"""
-    s = tx.dot(w)
-    l = np.log(1 + tx.dot(w))
-
+    loss = 0
     for i in range(len(y)):
-        if np.isinf(l[i]):
-            l[i] = s[i]
-
-    for i in range(len(y)):
-        if l[i] == 0:
-            l[i] = np.log(2) + s[i] / 2
-
-    sum = 0
-
-    for i in range(len(y)):
-        sum += l[i] - y[i]*(tx[i].dot(w))
-
-    return sum
+        loss += np.logaddexp(0,tx[i].dot(w))-y[i]*(tx[i].dot(w))
+    return loss
 
 def calculate_gradient(y, tx, w):
     """compute the gradient of loss."""
-    """return tx.T.dot((sigmoid(tx.dot(w))-y))"""
     return np.dot(tx.T, sigmoid(np.dot(tx, w)) - y)
 
 def learning_by_gradient_descent(y, tx, w, gamma):
-    """
-    Do one step of gradient descen using logistic regression.
-    Return the loss and the updated w.
-    """
-    w = w - gamma*calculate_gradient(y,tx,w)
+    """ Do one step of gradient descen using logistic regression.
+    Return the loss and the updated w. """
     loss = calculate_loss(y,tx,w)
+    grad = calculate_gradient(y,tx,w)
+    w = w - gamma*grad
     return loss, w
-
-from proj1_helpers import de_standardize
 
 def logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
     # init parameters
-    """threshold = 1e-8
-    losses = []
-
-    # build tx
-    # tx = np.c_[np.ones((y.shape[0], 1)), x]
-    # w = np.zeros((tx.shape[1], 1))
+    threshold = 1e-8
     w = initial_w
+    y = (y+1)/2
+    losses = []
 
     # start the logistic regression
     for iter in range(max_iters):
         # get loss and update w.
         loss, w = learning_by_gradient_descent(y, tx, w, gamma)
-        # log info
-        # if iter % 100 == 0:
-            # print("Current iteration={i}, loss={l}".format(i=iter, l=loss))
-        # converge criterion
         losses.append(loss)
+        
+        # converge criteria
         if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
             break
-    # visualization
-    # visualization(y, x, mean_x, std_x, w, "classification_by_logistic_regression_gradient_descent")
-    # print("loss={l}".format(l=calculate_loss(y, tx, w)))
-    return w, loss"""
-
-    if (initial_w is None):
-            initial_w = np.zeros(tx.shape[1])
-
-    w = initial_w
-    y = (1 + y) / 2
-    losses = []
-    threshold = 0.1
-
-        # start the logistic regression
-    for iter in range(max_iters):
-            # get loss and update w.
-            w, loss = learning_by_gradient_descent(y, tx, w, gamma)
-            losses.append(loss)
-
-            # converge criteria
-            if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
-                break
-
+            
     return w, loss
+
+
     
     
-"""
-Penalized Logistic regression
-"""
+""" Logistic regression using Newton's method """
+
+def calculate_hessian(y, tx, w):
+    """return the hessian of the loss function."""
+    pred = sigmoid(tx.dot(w))
+    pred = np.diag(pred.T[0])
+    S = np.multiply(pred,(1-pred))
+    return tx.T.dot(S).dot(tx)
+
+def logistic_regression_calculation(y, tx, w):
+    """return the loss, gradient, and hessian."""
+    loss = calculate_loss(y,tx,w)
+    gradient = calculate_gradient(y,tx,w)
+    hessian = calculate_hessian(y,tx,w)
+    return loss, gradient, hessian
+
+def learning_by_newton_method(y, tx, w):
+    """ Do one step on Newton's method.
+    return the loss and updated w. """
+    loss, gradient, hessian = logistic_regression_calculation(y,tx,w)
+    w = w - np.linalg.solve(hessian,gradient)
+    return loss, w
+
+def logistic_regression_newton_method(y, tx, lambda_, initial_w, max_iters, gamma):
+    # init parameters
+    threshold = 1e-8
+    w = initial_w
+    y = (y+1)/2
+    losses = []
+
+    # start the logistic regression
+    for iter in range(max_iter):
+        # get loss and update w.
+        loss, w = learning_by_newton_method(y, tx, w)
+        losses.append(loss)
+            
+        # converge criterion
+        if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
+            break
+    return w, loss
+
+
+
+""" Regularized logistic regression using gradient descent """
 
 def penalized_logistic_regression(y, tx, w, lambda_):
     """return the loss, gradient, and hessian."""
@@ -222,10 +215,8 @@ def penalized_logistic_regression(y, tx, w, lambda_):
     return loss, gradient
 
 def learning_by_penalized_gradient(y, tx, w, gamma, lambda_):
-    """
-    Do one step of gradient descent, using the penalized logistic regression.
-    Return the loss and updated w.
-    """
+    """ Do one step of gradient descent, using the penalized logistic regression.
+    Return the loss and updated w. """
     loss, gradient = penalized_logistic_regression(y,tx,w,lambda_)
     w = w - gamma*gradient
     return loss, w
@@ -233,26 +224,19 @@ def learning_by_penalized_gradient(y, tx, w, gamma, lambda_):
 def reg_logistic_regression(y, tx, lambda_ , initial_w, max_iters, gamma):
     threshold = 1e-8
     losses = []
-
-    # build tx
-    # tx = np.c_[np.ones((y.shape[0], 1)), x]
-    # w = np.zeros((tx.shape[1], 1))
     w = initial_w
+    y = (y+1)/2
 
     # start the logistic regression
-    for iter in range(max_iter):
+    for iter in range(max_iters):
         # get loss and update w.
         loss, w = learning_by_penalized_gradient(y, tx, w, gamma, lambda_)
-        # log info
-        # if iter % 100 == 0:
-            # print("Current iteration={i}, loss={l}".format(i=iter, l=loss))
-        # converge criterion
         losses.append(loss)
+        
+        # converge criterion
         if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
             break
-    # visualization
-    # visualization(y, x, mean_x, std_x, w, "classification_by_logistic_regression_penalized_gradient_descent")
-    # print("loss={l}".format(l=calculate_loss(y, tx, w)))
+            
     return w, loss
 
 
